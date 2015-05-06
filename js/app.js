46b55eb95dac5a7e0709cbdc4a95871e233254dd
Alvaro Molina <alvaromolinac@gmail.com>
2015-05-05 21:19:05 -0400

    init

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var app = angular.module('beersapp',[]);
+var app = angular.module('beersapp',['ngRoute']);
+
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'views/beers.html',
+		controller: 'beerlistController'
+	});
+}]);
+
 
 app.controller('beerlistController', function($scope){
   $scope.beers = [{name: 'taquina', avb: 6},{name: 'pacena', avb: 5},{name: 'huari', avb: 8}];
 });
+
